cd3affa3bc4b37f9e3de69c54daa1b31dfd0affa
Max Fain <maximofain0@gmail.com>
2025-01-25 23:37:50 -0300

    Add Button first logic

---
+++ Scripts/Button.gd
@@ -0,0 +1,27 @@
+extends StaticBody2D
+
+var UNPRESSED_SPRITE = preload("res://Sprites/Buttons/UnpressedButton.png")
+var PRESSED_SPRITE = preload("res://Sprites/Buttons/PressedButton.png")
+var RED_COLOR : Color = Color("ff0000")
+var GREEN_COLOR: Color = Color("009b00")
+
+var isButtonPressed: bool = false
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if isButtonPressed:
+		$Sprite2D.texture = PRESSED_SPRITE
+		$PointLight2D.color = GREEN_COLOR
+	else:
+		$Sprite2D.texture = UNPRESSED_SPRITE
+		$PointLight2D.color = RED_COLOR
+
+func _on_button_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Box"):
+		isButtonPressed = true
+		#GATE.OPEN
+
+func _on_button_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("Box"):
+		isButtonPressed = false
+		#GATE.CLOSE
